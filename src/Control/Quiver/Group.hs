67876d4f210a0450511a43f16ad913f694aca170
Patryk Zadarnowski <pat@jantar.org>
2016-04-05 12:08:08 +1000

    Update Group.hs
    How about the above? The finaliser stuff is already serviced adequately by >>! for cases where 'p' happens to be a Maybe type (so that SQ a g m (Maybe p) becomes an SP).
    
    I also see your point re allowing accum to fail, but maybe that's better accomplished in the underlying monad
    then with spfail? (in which case it'd be good to have separate monadic spaccumM and a pure spaccum for performance)
    
    I also flipped the arguments to make it more fold-like.
    
    I'd love to add these to the base quiver module, since they're pretty fundamental and don't require any additions. If you send me a pull request, I'll approve it, and feel free to add yourself to the author/copyright list, since right now you're doing more work on quiver than I do!

--- src/Control/Quiver/Group.hs
+++ src/Control/Quiver/Group.hs
@@ -16,32 +16,16 @@ import Control.Quiver.SP
 --------------------------------------------------------------------------------
 
 -- | Accumulate values within a Quiver.
-spaccum :: (a -> p)
-              -- ^ Create the initial partial accumulation @p@.
-           -> (p -> a -> Either p (g, Maybe a))
-              -- ^ Attempt to add a new value to a partial
-              -- accumulation; returns either an updated partial
-              -- accumulation or else a completed accumulation @g@ as
-              -- well as optionally the initial value (if it was /not/
-              -- added to the completed accumulation).
-           -> (p -> Maybe g)
-              -- ^ Attempt to convert the final partial accumulation
-              -- to a complete accumulation.  If this function returns
-              -- @'Nothing'@ then the final partial accumulation is
-              -- returned using 'spfailed'.
-           -> SP a g m p
-spaccum mkInit addA finalise = createNewAccum
+spaccumM :: Monad m
+           => (p -> a -> m (Maybe g, p))
+              -- ^ Adds new value to a partial accumulation;
+              -- returns an optional completed accumulation,
+              -- and an updated partial accumulation.
+           -> p
+              -- ^ Initial partial accumulation @p@.
+           -> SQ a g m (Maybe p)
+spaccumM accum = loopc
   where
-    createNewAccum = spconsume newAccumFrom spcomplete
-
-    newAccumFrom = accumPartial . mkInit
-
-    accumPartial p = spconsume (withValue . addA p) (finalisePartial p)
-
-    withValue epa = case epa of
-                      Left p        -> accumPartial p
-                      Right (g,ma') -> produce g
-                                               (const (maybe createNewAccum newAccumFrom ma'))
-                                               spincomplete
-
-    finalisePartial p = maybe (spfailed p) spemit (finalise p)
+    loopc p = consume (loopd p) (deliver (Just p))
+    loopd p x = do (mb, p') <- accum p x
+                   maybe (loopc p') (\b -> produce b (const $ loopc p') (deliver Nothing)) mb
